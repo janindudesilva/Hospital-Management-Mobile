cfa3947144b679d854b2b11a0e4281f90f901d4d
Janindu de silva <janindudesilva10@gmail.com>
2026-04-28 13:09:04 +0530

    Delete src directory

--- src/routes/prediction.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { createPrediction } from '../controllers/prediction.controller.js';
-import { protect } from '../middleware/auth.middleware.js';
-
-const router = Router();
-router.post('/', protect, createPrediction);
-export default router;
--- src/utils/jwt.js
+++
@@ -1,7 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-export const signToken = (payload) => {
-  return jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || '7d'
-  });
-};
